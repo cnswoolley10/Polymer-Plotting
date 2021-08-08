74adcb0603fec81014e3ab7392e4d9c9c5b5f56f
CNSWoolley <74056654+cnswoolley10@users.noreply.github.com>
2021-08-08 17:07:36 +0100

    Create executable version

---
+++ PolymerPlottingGUI_Executable/PolymerPlottingGUI.cmd
@@ -0,0 +1,6 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'PolymerPlottingGUI.exe' with Python DLL being found.
+set PATH=c:\users\chris\appdata\local\programs\python\python39;%PATH%
+"%~dp0.\PolymerPlottingGUI.exe"
+
