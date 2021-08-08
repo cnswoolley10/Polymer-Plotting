be609256248eebcd2acf7d3a6dcb624ccb526209
CNSWoolley <74056654+cnswoolley10@users.noreply.github.com>
2021-08-08 14:11:34 +0100

    separate GUI Building to separate file 1

--- Code/PolymerPlottingGUI.py
+++ Code/PolymerPlottingGUI.py
@@ -1,28 +1,15 @@
 import PySimpleGUI as sg
 import numpy as np
 from polymer_chains import *
+import GUI_tools as GUI
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 from itertools import count
 import os
 
 # ------------------------------- This is to include a matplotlib figure in a Tkinter canvas
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
-def draw_figure_w_toolbar(canvas, fig, canvas_toolbar):
-    if canvas.children:
-        for child in canvas.winfo_children():
-            child.destroy()
-    if canvas_toolbar.children:
-        for child in canvas_toolbar.winfo_children():
-            child.destroy()
-    figure_canvas_agg = FigureCanvasTkAgg(fig, master=canvas)
-    figure_canvas_agg.draw()
-    toolbar = Toolbar(figure_canvas_agg, canvas_toolbar)
-    toolbar.update()
-    figure_canvas_agg.get_tk_widget().pack(side='right', fill='both', expand=1)
-class Toolbar(NavigationToolbar2Tk):
-    def __init__(self, *args, **kwargs):
-        super(Toolbar, self).__init__(*args, **kwargs)
+
+GUI.setup_MPL_for_GUI()
 
 # ------------------------------- PySimpleGUI CODE
 # define chain options for dropdown
