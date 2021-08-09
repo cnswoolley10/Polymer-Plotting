550ea00ebbe10f9a5aae40f2466780981dc67562
CNSWoolley <74056654+cnswoolley10@users.noreply.github.com>
2021-08-09 18:23:38 +0100

    Try auto-py-to-exe as a packaging tool

--- PolymerPlottingGUI_Executable/PolymerPlottingGUI.cmd
+++
@@ -1,6 +0,0 @@
-
-@echo off
-rem This script was created by Nuitka to execute 'PolymerPlottingGUI.exe' with Python DLL being found.
-set PATH=c:\users\chris\appdata\local\programs\python\python39;%PATH%
-"%~dp0.\PolymerPlottingGUI.exe"
-
