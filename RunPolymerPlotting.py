3f015613e705a987fdc147d50ea6e0f256513e57
CNSWoolley <74056654+cnswoolley10@users.noreply.github.com>
2021-08-21 12:02:35 +0100

    Create RunPolymerPlotting.py
    A file to run the polymer plotting GUI once the package is installed from PyPI

---
+++ RunPolymerPlotting.py
@@ -0,0 +1,7 @@
+# This file should open the PolymerPlottingGUI if the PolymerPlotting package is correctly installed
+
+# First import the GUI module
+from PolymerPlotting import PolymerPlottingGUI as PPG
+
+# Then run the GUI
+PPG.main()
